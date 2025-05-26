773a4a1ba64c19a2ac99fe94935b0758f2141a12
Benjamin Legrand <benjilegnard@protonmail.com>
2025-05-26 22:18:27 +0200

    feat: add example folder, and multiple entrypoints in vite config.
    - just put an index.html in each folders in examples/ and it will be
    built with vite

---
+++ examples/01-events/main.ts
@@ -0,0 +1,3 @@
+console.log("COUCOU");
+
+const test = new Observable(() => {});
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,26 @@
 import { defineConfig, PluginOption } from "vite";
 import preact from "@preact/preset-vite";
+import { dirname, resolve, join } from "node:path";
+import { readdirSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/** returns an object with all entry points ( main index.html and examples folder) */
+const getFilesInput = () => {
+  const results = readdirSync(join(__dirname, "examples"));
+  return results.reduce(
+    (acc, val) => {
+      acc[val] = resolve(__dirname, `examples/${val}/index.html`);
+      console.log("val", val);
+      return acc;
+    },
+    // initial value of accumulator
+    {
+      main: resolve(__dirname, "index.html"),
+    },
+  );
+};
 
 /** reload markdown file on changes */
 const CustomHmr: () => PluginOption = () => ({
@@ -9,7 +30,6 @@ const CustomHmr: () => PluginOption = () => ({
   handleHotUpdate({ file, server }) {
     if (file.endsWith(".md")) {
       console.log("markdown file changed, reloading...");
-
       server.ws.send({
         type: "full-reload",
         path: "*",
@@ -18,4 +38,11 @@ const CustomHmr: () => PluginOption = () => ({
   },
 });
 
-export default defineConfig({ plugins: [CustomHmr(), preact()] });
+export default defineConfig({
+  plugins: [CustomHmr(), preact()],
+  build: {
+    rollupOptions: {
+      input: getFilesInput(),
+    },
+  },
+});
